b20fcab69f92da7859fd3b54921b1b43baaaf1f1
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2023-01-29 13:51:02 +0100

    add db

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,68 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS user;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+CREATE TABLE user (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  firstname varchar(100) NOT NULL,
+  lastname varchar(100) NOT NULL,
+  email varchar(200) NOT NULL,
+  city varchar(100),
+  avatar varchar(255),
+  hashedPassword varchar(100) NOT NULL,
+  date_creation DATETIME NOT NULL DEFAULT NOW() 
+);
+
+INSERT INTO user (firstname, lastname, email, city, avatar, hashedPassword, date_creation) VALUES ('Iris', 'Succi', 'iris@gmail.com', 'Cannes', 'iris.jpg', '12345!', '2019-01-01 00:00:00'),('Madeline', 'Thomas', 'madeline@gmail.com', 'Los Angeles', 'madeline.jpg', '12345!','2019-01-01 00:00:00');
+
+DROP TABLE IF EXISTS spot;
+
+CREATE TABLE spot (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name varchar(100) NOT NULL,
+  city varchar(100) NOT NULL,
+  country varchar(100) NOT NULL,
+  latitude varchar(100) NOT NULL,
+  longitude varchar(100) NOT NULL,
+  picture TEXT,
+  type varchar(100) NOT NULL,
+  level varchar(100) NOT NULL,
+  prevision TEXT,
+  webcam TEXT
+);
+
+INSERT INTO spot (name, city, country, latitude, longitude, picture, type, level, prevision, webcam) VALUES ('Uluwatu', 'Uluwatu', 'Bali', '-8.8197', '115.0918', 'uluwatu.jpg', 'reef', 'expert', 'https://magicseaweed.com/Uluwatu-Surf-Report/1/', 'https://www.surfline.com/surf-report/uluwatu/5842041f4e65fad6a7708a0d'),('Teahupoo', 'Teahupoo', 'Tahiti', '-17.5383', '-149.5667', 'teahupoo.jpg', 'reef', 'expert', 'https://magicseaweed.com/Teahupoo-Surf-Report/1/', 'https://www.surfline.com/surf-report/teahupoo/5842041f4e65fad6a7708a0e'),('Pipeline', 'Pipeline', 'Hawaii', '21.9817', '-159.3717', 'pipeline.jpg', 'reef', 'expert', 'https://magicseaweed.com/Pipeline-Surf-Report/1/', 'https://www.surfline.com/surf-report/pipeline/5842041f4e65fad6a7708a0f'),('La cote des basques', 'Biarritz', 'France', '43.4833', '-1.5667', 'biarritz.jpg', 'beach', 'beginner', 'https://magicseaweed.com/Biarritz-Surf-Report/1/', 'https://www.surfline.com/surf-report/biarritz/5842041f4e65fad6a7708a10'),('La Gravière', 'La Gravière',  'France', '43.4833', '-1.5667', 'lagraviere.jpg', 'beach', 'beginner', 'https://magicseaweed.com/La-Graviere-Surf-Report/1/', 'https://www.surfline.com/surf-report/la-graviere/5842041f4e65fad6a7708a11'),('La Torche', 'La Torche', 'France', '48.4833', '-4.5667', 'latorche.jpg', 'beach', 'beginner', 'https://magicseaweed.com/La-Torche-Surf-Report/1/', 'https://www.surfline.com/surf-report/la-torche/5842041f4e65fad6a7708a12'); 
+
+DROP TABLE IF EXISTS comment;
+
+CREATE TABLE comment (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  comment TEXT NOT NULL,
+  id_user int,
+  id_spot int,
+  date_creation DATETIME NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (id_user) REFERENCES user(id),
+  FOREIGN KEY (id_spot) REFERENCES spot(id)
+);
+
+INSERT INTO comment (id_user, id_spot, comment, date_creation) VALUES (1, 1, 'Super spot belle gauche !', '2019-01-01 00:00:00'),(1, 2, 'Super spot meme si j ai cassé ma board !', '2019-01-01 00:00:00'),(1, 3, 'Super spot attention rocher!', '2019-01-01 00:00:00'),(1, 4, 'Parfait', '2019-01-01 00:00:00'),(1, 5, 'Meilleur spot', '2019-01-01 00:00:00'),(1, 6, 'Parfait pour les débutant', '2019-01-01 00:00:00'),(2, 1, 'Trop beau spot', '2019-01-01 00:00:00'),(2, 2, 'Super spot !', '2019-01-01 00:00:00'),(2, 3, 'Super spot !', '2019-01-01 00:00:00'),(2, 4, 'Vague qui deferle longtemps', '2019-01-01 00:00:00'),(2, 5, 'Attention au rocher', '2019-01-01 00:00:00'),(2, 6, 'Super spot !', '2019-01-01 00:00:00');
+
+DROP TABLE IF EXISTS favorite;
+
+CREATE TABLE favorite (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_user int,
+  id_spot int,
+  date_creation DATETIME NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (id_user) REFERENCES user(id),
+  FOREIGN KEY (id_spot) REFERENCES spot(id)
+);
+
+DROP TABLE IF EXISTS spot_visited;
+
+CREATE TABLE spot_visited (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id_user int,
+  id_spot int,
+  date_creation DATETIME NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (id_user) REFERENCES user(id),
+  FOREIGN KEY (id_spot) REFERENCES spot(id)
+);
